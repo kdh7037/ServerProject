b84ce6d671987b4d9f79bdadf9d24ad5f4b37bbf
kdh7037 <kdh7037@naver.com>
2022-07-26 10:39:20 +0900

    세부 구조 정리

--- ServerProject/Database.cpp
+++ ServerProject/Database.cpp
@@ -7,8 +7,8 @@ void Database::Connect() {
 	MYSQL mysql;
 	mysql_init(&mysql);
 	if (!mysql_real_connect(&mysql, "localhost", "root", "0000", "mysql", 3306, NULL, 0))
-		cout << "error";
+		cout << "DB error" << endl;
 	else
-		cout << "success";
+		cout << "DB connect" << endl;
 	mysql_close(&mysql);
 }
